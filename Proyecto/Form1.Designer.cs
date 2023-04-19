c912d89b9123dfef48d59f91e2ca6f7a899f9214
jose chambi <jose.chambi@ingrammicro.com>
2023-04-19 10:43:12 -0500

    first commit

---
+++ Proyecto/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPara = new System.Windows.Forms.TextBox();
+            this.txtCopia = new System.Windows.Forms.TextBox();
+            this.txtCopiaOculta = new System.Windows.Forms.TextBox();
+            this.txtAsunto = new System.Windows.Forms.TextBox();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAdjunto = new System.Windows.Forms.TextBox();
+            this.btnAdjunto = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(38, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Para:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(38, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CC:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(38, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CCO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(38, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Asunto:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(38, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Mensaje:";
+            // 
+            // txtPara
+            // 
+            this.txtPara.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPara.Location = new System.Drawing.Point(41, 41);
+            this.txtPara.Name = "txtPara";
+            this.txtPara.Size = new System.Drawing.Size(545, 20);
+            this.txtPara.TabIndex = 1;
+            // 
+            // txtCopia
+            // 
+            this.txtCopia.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCopia.Location = new System.Drawing.Point(41, 85);
+            this.txtCopia.Name = "txtCopia";
+            this.txtCopia.Size = new System.Drawing.Size(545, 20);
+            this.txtCopia.TabIndex = 2;
+            // 
+            // txtCopiaOculta
+            // 
+            this.txtCopiaOculta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCopiaOculta.Location = new System.Drawing.Point(41, 131);
+            this.txtCopiaOculta.Name = "txtCopiaOculta";
+            this.txtCopiaOculta.Size = new System.Drawing.Size(545, 20);
+            this.txtCopiaOculta.TabIndex = 3;
+            // 
+            // txtAsunto
+            // 
+            this.txtAsunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAsunto.Location = new System.Drawing.Point(41, 175);
+            this.txtAsunto.Name = "txtAsunto";
+            this.txtAsunto.Size = new System.Drawing.Size(545, 20);
+            this.txtAsunto.TabIndex = 4;
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensaje.Location = new System.Drawing.Point(41, 270);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(545, 149);
+            this.txtMensaje.TabIndex = 8;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnEnviar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEnviar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnviar.ForeColor = System.Drawing.Color.White;
+            this.btnEnviar.Location = new System.Drawing.Point(429, 425);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(157, 23);
+            this.btnEnviar.TabIndex = 9;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(38, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Adjunto:";
+            // 
+            // txtAdjunto
+            // 
+            this.txtAdjunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdjunto.Location = new System.Drawing.Point(41, 220);
+            this.txtAdjunto.Name = "txtAdjunto";
+            this.txtAdjunto.ReadOnly = true;
+            this.txtAdjunto.Size = new System.Drawing.Size(433, 20);
+            this.txtAdjunto.TabIndex = 7;
+            // 
+            // btnAdjunto
+            // 
+            this.btnAdjunto.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnAdjunto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdjunto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdjunto.ForeColor = System.Drawing.Color.White;
+            this.btnAdjunto.Location = new System.Drawing.Point(480, 218);
+            this.btnAdjunto.Name = "btnAdjunto";
+            this.btnAdjunto.Size = new System.Drawing.Size(47, 23);
+            this.btnAdjunto.TabIndex = 5;
+            this.btnAdjunto.Text = "+";
+            this.btnAdjunto.UseVisualStyleBackColor = false;
+            this.btnAdjunto.Click += new System.EventHandler(this.btnAdjunto_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(533, 218);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(53, 23);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "x";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(81, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(195, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "(Separe con coma \",\" para más de uno)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(81, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(195, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "(Separe con coma \",\" para más de uno)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(81, 113);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(195, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "(Separe con coma \",\" para más de uno)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(623, 478);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAdjunto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.txtAdjunto);
+            this.Controls.Add(this.txtAsunto);
+            this.Controls.Add(this.txtCopiaOculta);
+            this.Controls.Add(this.txtCopia);
+            this.Controls.Add(this.txtPara);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Enviar Correo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPara;
+        private System.Windows.Forms.TextBox txtCopia;
+        private System.Windows.Forms.TextBox txtCopiaOculta;
+        private System.Windows.Forms.TextBox txtAsunto;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAdjunto;
+        private System.Windows.Forms.Button btnAdjunto;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Proyecto/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto
+{
+    public partial class Form1 : Form
+    {
+        List<string> ArchivoAdjuntos = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //CONFIGURAR VALORES
+                string Host = "smtp.gmail.com";
+                int Puerto = 587;
+                string Usuario = "(INGRESE CORREO AQUI)";
+                string Clave = "(INGRESE CONTRASEÑA AQUI)";//clave generada para aplicación en GMAIL
+
+                //PROPORCIONAMOS AUTENTICACION DE GMAIL
+                SmtpClient smtp = new SmtpClient(Host, Puerto);
+                MailMessage msg = new MailMessage();
+
+
+                //CREAMOS EL CONTENIDO DEL CORREO
+                string[] Destinatario = txtPara.Text.Split(',');
+                string[] DestinatarioCopia = txtCopia.Text.Split(',');
+                string[] DestinatarioCopiaOculta = txtCopiaOculta.Text.Split(',');
+
+
+                msg.From = new MailAddress(Usuario, "SERVIDOR CORREO");
+                foreach (string correo in Destinatario) if (correo != "") msg.To.Add(new MailAddress(correo));
+                foreach (string correo in DestinatarioCopia) if (correo != "") msg.CC.Add(new MailAddress(correo));
+                foreach (string correo in DestinatarioCopiaOculta) if (correo != "") msg.Bcc.Add(new MailAddress(correo));
+                foreach (string adjunto in ArchivoAdjuntos) if (adjunto != "") msg.Attachments.Add(new Attachment(adjunto));
+                msg.Subject = txtAsunto.Text;
+                msg.IsBodyHtml = false;
+                msg.Body = txtMensaje.Text;
+
+
+                //ENVIAMOS EL CORREO
+                smtp.Credentials = new NetworkCredential(Usuario, Clave);
+                smtp.EnableSsl = true;
+                smtp.Send(msg);
+                MessageBox.Show("Mensaje Enviado");
+            }
+            catch
+            {
+                MessageBox.Show("Error al enviar");
+            }
+           
+        }
+
+        private void btnAdjunto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if(ofd.ShowDialog() == DialogResult.OK) {
+                foreach(string adjunto in ofd.SafeFileNames)
+                {
+                    txtAdjunto.Text = txtAdjunto.Text + adjunto + " | ";
+                }
+                foreach (string adjunto in ofd.FileNames)
+                {
+                    ArchivoAdjuntos.Add(adjunto);
+                }
+                
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            ArchivoAdjuntos = new List<string>();
+            txtAdjunto.Text = "";
+        }
+    }
+}
